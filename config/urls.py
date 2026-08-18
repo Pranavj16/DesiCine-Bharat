d84b5c092c2611cf3ec24fa324d3e6d0a7c9e839
Pranavj16 <pranavj162008@gmail.com>
2026-08-18 07:42:46 +0530

    feat(ui): add cinema favicon and /favicon.ico routing

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
     path('', include('theaters.urls')),
@@ -10,3 +14,6 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('dashboard/', include('admin_dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
